a23ec62875f3d69630a4e70f8c4e8c011706c82e
BVanBuren <bvanburenwx@gmail.com>
2021-08-15 23:10:16 -0400

    completed the test for employee.js

--- _Tests_/employee.test.js
+++ _Tests_/employee.test.js
@@ -2,7 +2,7 @@
 const Employee = require(lib/Employee.js);
 
 // Test for the Employee object
-test('creates an object for employee', () => {
+it('creates an object for employee', () => {
     const employee = new Employee('Brandon', 654, 'bvanburenwx@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
@@ -11,8 +11,27 @@ test('creates an object for employee', () => {
 });
 
 // Test for getting Name from getName function from employee.js
-test('gets employee name', () => {
+it('gets employee name', () => {
     const employee = new Employee('Brandon', 654, 'bvanburenwx@gmail.com')
 
     expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// Test for getting id from getID function from employee.js
+it('gets employee id from input', () => {
+    const employee = new Employee('Brandon', 654, 'bvanburenwx@gmail.com')
+
+    expect(employee.getId()). toEqual(expect.any(Number));
+});
+
+it('get employee email from input', () => {
+    const employee =new Employee('Brandon', 654, 'bvanburenwx@gmail.com')
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+it('gets role from employee upon input', () => {
+    const employee = new Employee('Brandon', 654, 'bvanburenwx@gmail.com')
+
+    expect(employee.getRole()).toEqual('Employee');
 });

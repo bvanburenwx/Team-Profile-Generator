a440e7321b9b2498f8dc3a8d5ad35eea5abfbeec
BVanBuren <bvanburenwx@gmail.com>
2021-09-05 13:46:39 -0400

    added some styling to the html that will be generated. changed some of the different js files to make sure they link up correctly. Program is running correctly

--- index.js
+++ index.js
@@ -1,78 +1,161 @@
 // const generatedHTML = require('./src/generateHTML');
 // access file system and inquirer to generate questions and create HTML
-const fs = require('fs');
-const inquirer = require('inquirer');
-const path = require('path');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
 // profiles
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const outputDirectory = path.resolve(__dirname, 'dist');
-const outputPath = path.join(outputDirectory, 'team.html');
-const render = require('./src/template.js');
+const outputDirectory = path.resolve(__dirname, "dist");
+const outputPath = path.join(outputDirectory, "team.html");
+const render = require("./src/template.js");
 
 // empty array for team
 const inputTeam = [];
 
-function mainMenu(){
-
-function createManager(){
-inquirer.prompt( [
+function mainMenu() {
+  function createManager() {
+    inquirer
+      .prompt([
         {
-            type: 'input',
-            name: 'name',
-            message: 'Please enters the team manager name.',
-        },
-
-        {
-            type: 'input', 
-            name: 'id',
-            message: 'Please enter the manager ID.', 
-        },
-        {   
-            type: 'input',
-            name: 'email', 
-            message: 'Please enter the managers email.',
+          type: "input",
+          name: "name",
+          message: "Please enters the team manager name.",
         },
 
         {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'Please enter the managers office number.',
+          type: "input",
+          name: "id",
+          message: "Please enter the manager ID.",
         },
-    ])
-    .then(managerInput => {
+        {
+          type: "input",
+          name: "email",
+          message: "Please enter the managers email.",
+        },
+
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "Please enter the managers office number.",
+        },
+      ])
+      .then((managerInput) => {
         const { name, id, email, officeNumber } = managerInput;
-        const manager = new Manager (name, id, email, officeNumber);
+        const manager = new Manager(name, id, email, officeNumber);
 
         inputTeam.push(manager);
         console.log(manager);
         init();
-    });
-
-}
-    function renderTeam() {
-       if(!fs.existsSync(outputDirectory)){
-           fs.mkdirSync(outputDirectory)
-       }
-       fs.writeFileSync(outputPath, render(inputTeam), "utf-8");
+      });
+  }
+  function renderTeam() {
+    if (!fs.existsSync(outputDirectory)) {
+      fs.mkdirSync(outputDirectory);
     }
-    function init() {
-        inquirer.prompt(
-            {
-                type: 'choice', 
-                name: 'userChoice',
-                message: 'What type of team member would you like to add?', 
-                choices: [
-                    "Engineer",
-                    "Intern",
-                    "I dont want to add anymore team members."
-                ]
-            },
-        )
-    }
+    fs.writeFileSync(outputPath, render(inputTeam), "utf-8");
+  }
+  function init() {
+    inquirer.prompt({
+      type: "list",
+      name: "userChoice",
+      message: "What type of team member would you like to add?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I dont want to add anymore team members.",
+      ],
+    }).then(answer => {
+        console.log('this is form init', answer.userChoice);
+        switch(answer.userChoice){
+            case 'Engineer':
+                createEngineer();
+                break;
 
-    createManager();
+            case 'Intern':
+                createIntern();
+                break;
+
+            default: 
+            renderTeam();
+        }
+        
+    }) 
+  }
+  function createEngineer() {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the engineer's name.",
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the engineer's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the engineer's email.",
+      },
+
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Please enter the engineer's github.",
+      },
+    ])
+    .then((engineerInput) => {
+        const { name, id, email, gitHub } = engineerInput;
+        const engineer = new Engineer(name, id, email, gitHub);
+
+        inputTeam.push(engineer);
+        console.log(engineer);
+        init();
+      });
+  }
+  function createIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please enters the intern's name.",
+        },
+
+        {
+          type: "input",
+          name: "id",
+          message: "Please enter the intern's ID.",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please enter the intern's email.",
+        },
+
+        {
+          type: "input",
+          name: "school",
+          message: "Please enter the intern's school.",
+        },
+      ])
+      .then((internInput) => {
+        const { name, id, email, school } = internInput;
+        const intern = new Intern(name, id, email, school);
+
+        inputTeam.push(intern);
+        console.log(intern);
+        init();
+      });
 }
-    mainMenu()
+
+
+  createManager();
+}
+mainMenu();
--- src/template.js
+++ src/template.js
@@ -1,59 +1,71 @@
-
 const generateTeam = (team) => {
-
   const generateManager = (manager) => {
     return `
-        <div class="card">
-        <div class="card-header">
-        <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title">${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-        <li>${manager.getId()}</li>
-        <li>${manager.getEmail()}</li>
-        <li>${manager.getOfficeNumber()}</li>
-        </div>
+        <div class="card custom-card">
+          <div class="card-header custom-content">
+            <h2 class="card-title custom-info-title">${manager.getName()}</h2>
+            <h3 class="card-title custom-info-title">${manager.getRole()}</h3>
+          </div>
+          <div class="list-group list-group-flush custom-content">
+            <li class="list-group-item custom-info">ID: ${manager.getId()}</li>
+            <li class="list-group-item custom-info">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item custom-info">Office Number: ${manager.getOfficeNumber()}</li>
+          </div>
         </div>`;
   };
 
   const generateEngineer = (engineer) => {
-    return `<div class="card">
-    <div class="card-header">
-    <h2 class="card-title">${engineer.getName()}</h2>
-    <h3 class="card-title">${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-    <li>${engineer.getId()}</li>
-    <li>${engineer.getEmail()}</li>
-    <li>${engineer.getGithub()}</li>
-    </div>
+    return `
+    <div class="card custom-card">
+      <div class="card-header custom-content">
+        <h2 class="card-title custom-info-title">${engineer.getName()}</h2>
+        <h3 class="card-title custom-info-title">${engineer.getRole()}</h3>
+      </div>
+      <div class="list-group list-group-flush custom-content">
+        <li class="list-group-item custom-info">ID: ${engineer.getId()}</li>
+        <li class="list-group-item custom-info">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item custom-info">GitHub: <a href="github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</a></li>
+      </div>
     </div>`;
   };
 
   const generateIntern = (intern) => {
-    return `<div class="card">
-    <div class="card-header">
-    <h2 class="card-title">${manager.getName()}</h2>
-    <h3 class="card-title">${manager.getRole()}</h3>
-    </div>
-    <div class="card-body">
-    <li>${manager.getId()}</li>
-    <li>${manager.getEmail()}</li>
-    <li>${manager.getschool()}</li>
-    </div>
+    return `
+    <div class="card custom-card">
+      <div class="card-header custom-content">
+        <h2 class="card-title custom-info-title">${intern.getName()}</h2>
+        <h3 class="card-title custom-info-title">${intern.getRole()}</h3>
+      </div>
+      <div class="list-group list-group-flush custom-content">
+        <li class="list-group-item custom-info">ID: ${intern.getId()}</li>
+        <li class="list-group-item custom-info">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item custom-info">School: ${intern.getSchool()}</li>
+      </div>
     </div>`;
   };
 
   const html = [];
 
-  html.push(team.filter(employee => employee.getRole() === "Manager").map(manager => generateManager(manager)));
-  html.push(team.filter(employee => employee.getRole() === "Engineer").map(engineer => generateManager(engineer)).join(''));
-  html.push(team.filter(employee => employee.getRole() === "Intern").map(intern => generateManager(intern)).join(''));
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
 
-
-
-  return html.join('');
-
+  return html.join("");
+  };
   module.exports = (team) => {
     return `
         <!DOCTYPE html>
@@ -62,13 +74,23 @@ const generateTeam = (team) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile</title>
 </head>
 <body>
-    <div class="container>
-    <div class="row>${generateTeam(team)}</div>
+  <header> 
+    <h1> My Team </h1>
+  </header>
+    <div class="container profile">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+          ${generateTeam(team)}
+        </div>
+      </div>
     </div>
 </body>
 </html>`;
   };
-};
+
